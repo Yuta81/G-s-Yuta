180a9c5dbef89daa585766c07c7b008b9dbd9198
Yuta81 <yutaharaikawa@hotmail.com>
2016-09-27 19:56:56 +0900

    Update geo2.php

--- php/geo2.php
+++ php/geo2.php
@@ -57,8 +57,8 @@ $book_title = $_POST['book_title'];
 <head>
     <meta charset="UTF-8">
     <title>Document</title>
-    <link rel="stylesheet" href="/stockin/css/search_result.css">
-    <link rel="stylesheet" href="/stockin/css/reset.css">
+    <link rel="stylesheet" href="/css/search_result.css">
+    <link rel="stylesheet" href="/css/reset.css">
     <script src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
     <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyC434MBbhe6MuEUVmTwJsCnp-jwL7grBYI&callback=initMap" async defer></script>
 </head>
