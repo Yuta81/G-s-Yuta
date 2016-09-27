4f60886eaef51128fc4b5545cb31468777a36149
Yuta81 <yutaharaikawa@hotmail.com>
2016-09-27 20:04:15 +0900

    Update autocomp_for_book.php

--- php/autocomp_for_book.php
+++ php/autocomp_for_book.php
@@ -1,8 +1,8 @@
 <?php
-    $db_user = 'root';
-    $db_pass = 'yuta81bb';
-    $db_host = 'localhost';
-    $db_name = 'stokin_db';
+    $db_user = 'b60ae5b716dd01';
+    $db_pass = 'd3e554b7';
+    $db_host = 'us-cdbr-iron-east-04.cleardb.net';
+    $db_name = 'heroku_43b33c2f7cca19e';
     $db_type = 'mysql';
 
 $connect = mysqli_connect($db_host,$db_user,$db_pass) or die('Could not connect to mysql server.' );
