d66bbbb9905e1f8aa111cccba09ec9c6d0395a31
Yuta81 <yutaharaikawa@hotmail.com>
2016-09-28 17:54:51 +0900

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <title>トップページ</title>
     <script src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
     <link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/themes/smoothness/jquery-ui.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.1/jquery.min.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js"></script>
     <script type='text/javascript'></script>
     
