a74775f51098ad3df5ad7a7e96eb4a6bd13fcd2b
Yuta81 <yutaharaikawa@hotmail.com>
2016-09-29 13:51:49 +0900

    Update each_store.php

--- php/each_store.php
+++ php/each_store.php
@@ -1,8 +1,8 @@
 <?php
 
-mb_language("Japanese");
-mb_internal_encoding("UTF-8");
-mb_http_output("UTF-8");
+// mb_language("Japanese");
+// mb_internal_encoding("UTF-8");
+// mb_http_output("UTF-8");
 
 include("include.php");
 
