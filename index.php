007ad102981c7bc8c66d3cfa495c12d254e0eda6
Yuta81 <yutaharaikawa@hotmail.com>
2016-10-05 20:31:36 +0900

    Update index.php

--- index.php
+++ index.php
@@ -36,11 +36,28 @@ session_start();
 </head>
 <body>
   
-  <?php
+<?php
+if($_SESSION){
     
-    require_once('php/general_user_log_status.php');
+?>
+
+ <p><a href="general_user_info.php?user=<?= e($_SESSION['general_user_name']) ?>"><?= e($_SESSION['general_user_name']) ?></a>さん（ログイン中）<a href="general_user_logout.php"><button>ログアウト</button></a></p>
+ 
+
+<?php
+        
+}else{
     
- ?>
+?>
+   
+   <a href="general_user_login.php"><button>ログイン</button></a>
+   <a href="email_register.php"><button>会員登録</button></a>
+   
+<?php
+   
+   }
+    
+?>
    
    <form action="php/book_select.php" method="post">
        
