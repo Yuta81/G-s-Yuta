cc8a48bf6d8aee7c48bbc6184474f4c0a9f9e580
Yuta81 <yutaharaikawa@hotmail.com>
2016-10-06 16:07:44 +0900

    Update author_publish_date_asc.php

--- php/author_publish_date_asc.php
+++ php/author_publish_date_asc.php
@@ -1,5 +1,4 @@
 <?php
-
 session_cache_limiter('private_no_expire');
 session_start();
 
@@ -173,20 +172,28 @@ function initMap(){
                       <li>価格:   <?= e($row['price']) ?> 円（税込）</li>
                       <li>発行日:  <?= e($row['publish_date']) ?></li>
                       <li>
-                          <form action="geo2.php" method="post">
+                         <form action="geo2.php" method="post">
                               <input type="submit" value="現在地から距離順で書店を検索">
                               <input type="hidden" name="book_title" value="<?= e($row['book_title']) ?>">
                               <input type="hidden" name="lat" class="lat">
                               <input type="hidden" name="lon" class="lon">
+                              <input type="hidden" name="author" value="<?= e($row['author']) ?>">
+                              <input type="hidden" name="publish_date" value="<?= e($row['publish_date']) ?>">
+                              <input type="hidden" name="price" value="<?= e($row['price']) ?>">
+                              <input type="hidden" name="img" value="<?= e($row['img']) ?>">
                           </form>
                       </li> 
                       <li></li>
                       <li></li>
                       <li>
-                          <form action="sta2.php" method="post">
+                           <form action="sta2.php" method="post">
                               <input type="text" name="station_name" class="station_name">
                               <input type="submit" value="駅名から検索" placeholder="駅名を入力">
                               <input type="hidden" name="book_title" value="<?= e($row['book_title']) ?>">
+                              <input type="hidden" name="author" value="<?= e($row['author']) ?>">
+                              <input type="hidden" name="publish_date" value="<?= e($row['publish_date']) ?>">
+                              <input type="hidden" name="price" value="<?= e($row['price']) ?>">
+                              <input type="hidden" name="img" value="<?= e($row['img']) ?>">
                           </form>
                       </li>
                       
